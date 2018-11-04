738ea5354647f79981e0a31ede0dbc01a9aae2f6
dpak2411 <deepak.manoharan@outlook.com>
2018-11-04 16:36:20 +0530

    changes to properties

--- services/src/test/java/it/fds/taskmanager/TaskServiceJPATest.java
+++ services/src/test/java/it/fds/taskmanager/TaskServiceJPATest.java
@@ -9,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import it.fds.taskmanager.dto.TaskDTO;
-
+import it.fds.taskmanager.model.Task;
+import it.fds.taskmanager.repository.TasksRepository;
 /**
  * Basic test suite to test the service layer, it uses an in-memory H2 database. 
  * 
@@ -28,8 +31,13 @@ public class TaskServiceJPATest extends Assert{
 
     @Autowired
     TaskService taskService;
+    @Autowired
+    private TasksRepository tasksRepo;
+    
+    //
     
     @Test
+    @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
     public void writeAndReadOnDB() {
         TaskDTO t = new TaskDTO();
         t.setTitle("Test task1");
@@ -40,6 +48,58 @@ public class TaskServiceJPATest extends Assert{
         List<TaskDTO> list = taskService.showList();
         assertEquals(1, list.size());
     }
+    
+    /**
+     * Test to find a task which has been saved
+     */
+    @Test
+    @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+    public void findtask() {
+        TaskDTO t = new TaskDTO();
+        t.setTitle("Test task1");
+        t.setStatus(TaskState.NEW.toString().toUpperCase());
+        TaskDTO t1 = taskService.saveTask(t);
+        TaskDTO tOut = taskService.findOne(t1.getUuid());
+        assertEquals("Test task1", tOut.getTitle());    
+        
+    }
+    
+    
+   
+    
+    /**
+     *To test if a status of the postponed task is set to POSTPONED 
+     * Bug in the method, the status remains as NEW , not set to postponed.
+     */
+    @Test
+    @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+    public void PostponeTask() {
+        TaskDTO t = new TaskDTO();
+        t.setTitle("Test task2");
+        t.setStatus(TaskState.NEW.toString().toUpperCase());
+        TaskDTO t1 = taskService.saveTask(t);      
+        Boolean t2=taskService.postponeTask(t1.getUuid(), 10);
+        assertEquals(t2,true);        
+    }
+    
+    /**
+     * To check if the task is postponed
+     * @author admin
+     *
+     */
+    @Test
+    @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+    public void ListPostoneTasks() {
+        TaskDTO t = new TaskDTO();       
+        t.setTitle("Test task3");
+        t.setStatus(TaskState.NEW.toString().toUpperCase());
+        TaskDTO t1 = taskService.saveTask(t);
+        taskService.postponeTask(t1.getUuid(), 10);
+        List<Task> list = tasksRepo.findTaskToRestore(); 
+        assertEquals(0, list.size());
+             
+    }
+    
 
     @EnableJpaRepositories
     @Configuration
